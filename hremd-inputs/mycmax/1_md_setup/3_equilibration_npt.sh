bb24acb4e8c7d38045eed4f7ddc20b076a367bd7
GregorySchwing <gschwing@uno.edu>
2021-07-07 17:05:42 -0500

    added comments for getting velocities into gromacs

--- hremd-inputs/mycmax/1_md_setup/3_equilibration_npt.sh
+++ hremd-inputs/mycmax/1_md_setup/3_equilibration_npt.sh
@@ -8,6 +8,8 @@
 ####NPT equilibration.
 # For velocities
 #-t nvt.cpt 
+# From manual - https://manual.gromacs.org/documentation/current/onlinehelp/gmx-grompp.html
+#Starting coordinates can be read from trajectory with -t. The last frame with coordinates and velocities will be read, unless #the -time option is used. Only if this information is absent will the coordinates in the -c file be used. Note that these #velocities will not be used when gen_vel = yes in your .mdp file. An energy file can be supplied with -e to read Nose-Hoover #and/or Parrinello-Rahman coupling variables.
 gmx grompp -f mdp/npt.mdp -c nvt.gro -r nvt.gro -p topol.top -t nvt.cpt -o npt.tpr
 # Checkpoint holds - thermostat/barostat variables, random number states and NMR time averaged data.
 # Only used for velocities
--- hremd-inputs/mycmax/1_md_setup/4_production.sh
+++ hremd-inputs/mycmax/1_md_setup/4_production.sh
@@ -7,6 +7,9 @@
 
 
 ####Generate a gromacs tpr for production run.
+#-t npt.cpt 
+# From manual - https://manual.gromacs.org/documentation/current/onlinehelp/gmx-grompp.html
+#Starting coordinates can be read from trajectory with -t. The last frame with coordinates and velocities will be read, unless #the -time option is used. Only if this information is absent will the coordinates in the -c file be used. Note that these #velocities will not be used when gen_vel = yes in your .mdp file. An energy file can be supplied with -e to read Nose-Hoover #and/or Parrinello-Rahman coupling variables.
 gmx grompp -f mdp/md.mdp -c npt.gro -t npt.cpt -p topol.top -o md.tpr
 # Checkpoint holds - thermostat/barostat variables, random number states and NMR time averaged data.
 # Only used for velocities
