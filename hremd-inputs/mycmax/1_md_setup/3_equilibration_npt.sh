2b4fc959c5a8e1e5d43fea3447923b829f57dcfa
GregorySchwing <gschwing@uno.edu>
2021-07-07 16:04:26 -0500

    added back chkpoint

--- hremd-inputs/mycmax/1_md_setup/3_equilibration_npt.sh
+++ hremd-inputs/mycmax/1_md_setup/3_equilibration_npt.sh
@@ -11,4 +11,4 @@ gmx grompp -f mdp/npt.mdp -c nvt.gro -r nvt.gro -p topol.top -o npt.tpr
 # Checkpoint holds - thermostat/barostat variables, random number states and NMR time averaged data.
 # Only used for restarting if hardware/software prematurely terminates
 # Cant use the checkpoint from nvt to continue npt equilibration
-gmx mdrun -cpt 5 -s npt.tpr -deffnm npt
+gmx mdrun -cpi nvt.cpt -cpt 5 -s npt.tpr -deffnm npt
