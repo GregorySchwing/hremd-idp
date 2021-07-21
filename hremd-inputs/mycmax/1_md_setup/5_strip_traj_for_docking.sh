875fccf285c774b192a71f971f5ba275f2a5dfc3
GregorySchwing <gschwing@uno.edu>
2021-07-20 20:56:41 -0500

    mdtraj full system twice

--- hremd-inputs/mycmax/1_md_setup/5_strip_traj_for_docking.sh
+++ hremd-inputs/mycmax/1_md_setup/5_strip_traj_for_docking.sh
@@ -8,12 +8,17 @@
 
 ####2 is Protein minus hydrogen, we center protein and output
 gmx trjconv -s md.tpr -f prod_center.trr -o prod_center_no_h.trr -center -pbc mol -ur compact <<EOF
-1
+2
 2
 EOF
 
-
-gmx trjconv -s md.tpr -f prod_center.trr -o firstFrame.pdb -center -pbc mol -ur compact -dump 0 <<EOF
-1
+gmx trjconv -s md.tpr -f prod_center.trr -o frameForAlignment.pdb -center -pbc mol -ur compact -dump 0 <<EOF
 2
+2
+EOF
+
+gmx trjconv -s frameForAlignment.pdb -f prod_center_no_h.trr -o prod_center_no_h_aligned.trr -fit progressive -center <<EOF
+0
+0
+0
 EOF
